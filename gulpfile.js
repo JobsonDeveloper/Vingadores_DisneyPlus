c60b3f57cef940a05d3a6090a72743570df44eb8
Jobson Oliveira <jobsondeveloper@gmail.com>
2025-01-27 16:43:38 -0300

    Correção da execução default do Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ function images() {
     .pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(images);
+exports.default = gulp.parallel(images, styles, scripts);
 
 exports.watch = function() {
     gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
